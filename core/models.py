85935ec231fc9b3e52c1fef5015d87b891480099
arman <mr.kazemi.ar@gmail.com>
2022-02-26 18:41:30 +0330

    base config

---
+++ core/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from .manager import BaseManager
+
+# Create your models here.
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    phone = models.CharField(max_length=13, unique=True)
+    full_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    REQUIRED_FIELDS = ['email']
+    USERNAME_FIELD = 'phone'
+
+    def __str__(self):
+        return self.full_name
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+
+class BaseModel(models.Model):
+    """
+        This model mixin usable for logical delete and logical activate status datas.
+    """
+    created = models.DateTimeField(auto_now_add=True, editable=False, )
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+    delete_timestamp = models.DateTimeField(null=True, blank=True)
+    deleted_at = models.DateTimeField(
+        null=True, blank=True,
+        verbose_name=_("Deleted Datetime"),
+        help_text=_("This is deleted datetime")
+    )
+    is_deleted = models.BooleanField(
+        default=False,
+        verbose_name=_("Deleted status"),
+        help_text=_("This is deleted status")
+    )
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name=_("Active status"),
+        help_text=_("This is active status")
+    )
+
+    # custom manager for get active items
+    objects = BaseManager()
+
+    class Meta:
+        abstract = True
+
+    def deleter(self):
+        self.deleted_at = timezone.now()
+        self.is_deleted = True
+        self.save()
+
+    def deactivate(self):
+        self.is_active = False
+        self.save()
+
+    def activate(self):
+        self.is_active = True
+        self.save()
